35bcafd7cc43bbb2ddd364fec8733be13bbbf927
ArtjomZagrebaylov <114191769+ArtjomZagrebaylov@users.noreply.github.com>
2022-09-23 13:57:25 +0300

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,9 +1,13 @@
 
 const checkTemp = (t) => {
+   
     if (Number(t) <= 20)
         return -1;
-    if (Number(t) >= 21)
-        return 0;
-    if (Number(t) <= 40)
+     
+   else if (Number(t) >= 40)
         return 1;
+    
+   else if (Number(t) >= 21)
+        return 0;
+
 }
