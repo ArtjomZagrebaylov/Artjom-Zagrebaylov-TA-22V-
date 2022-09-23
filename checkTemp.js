c29cff4f23b4ce6103d77a6e01aa5006086215e2
ArtjomZagrebaylov <114191769+ArtjomZagrebaylov@users.noreply.github.com>
2022-09-23 15:12:06 +0300

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,7 +1,10 @@
-/*let min = 5;
+let min = 5;
 let max = 100;
 let rand = Math.random();
-let randomNumber = Math.floor(rand*(max-min+1))+min;*/
+let randomNumber = Math.floor(rand*(max-min+1))+min;
+//1-20
+//21-40
+//41-60
 
 //let temp = 30;
 function checkTemp(temp)
@@ -11,11 +14,18 @@ function checkTemp(temp)
     if (temp <= 20)
         return  -1;
      
-   else if (temp >= 40)
+    if (temp > 40)
        return  1;
     
-   else if (temp >= 21)
+    if (temp >= 21)
         return  0;
 
 }
-console.log(checkTemp(5));
+console.log(checkTemp(1));//-1
+console.log(checkTemp(20));//-1
+console.log(checkTemp(21));//0
+console.log(checkTemp(40));//0
+console.log(checkTemp(41));//1
+console.log(checkTemp(60));//1
+console.log(checkTemp(61));//1
+console.log(checkTemp(randomNumber));//1
